3ad4e0042cd0b5274f813c5c9b0e7793a9bd6782
jabobinski <bobinski00@gmail.com>
2024-04-23 12:16:47 +0200

    .

--- tasks 1-20/z4.py
+++ tasks 1-20/z4.py
@@ -1,10 +1,8 @@
-# Define a function to modify the string as described
 def replace(b):
     c = b[0]
     d = c + b[1:].replace(c, '$')
     return d
 
-# Example usage:
 a = 'restart'
 result = replace(a)
 print("Sample String:", a)
