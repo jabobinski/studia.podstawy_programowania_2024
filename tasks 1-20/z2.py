8848aa7da8fb47399b382977545b91f26034e143
jabobinski <bobinski00@gmail.com>
2024-04-23 12:08:24 +0200

    .

--- tasks 1-20/z2.py
+++ tasks 1-20/z2.py
@@ -4,7 +4,6 @@ def count_characters(string):
         char_freq[char] = char_freq.get(char, 0) + 1
     return char_freq
 
-# Example usage:
 sample_string = "google.com"
 result = count_characters(sample_string)
 print("Character frequency:", result)
