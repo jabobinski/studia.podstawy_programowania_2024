7feaa311ea7a910f83d5ae63a8bc2677ee79efcc
jabobinski <bobinski00@gmail.com>
2024-04-23 12:10:00 +0200

    poprawka

--- tasks 1-20/z2.py
+++ tasks 1-20/z2.py
@@ -1,9 +1,9 @@
-def count_characters(string):
+def count_chars(string):
     char_freq = {}
     for char in string:
         char_freq[char] = char_freq.get(char, 0) + 1
     return char_freq
 
-sample_string = "google.com"
-result = count_characters(sample_string)
+website = "google.com"
+result = count_chars(website)
 print("Character frequency:", result)
